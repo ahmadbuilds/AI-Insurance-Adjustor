19f0aef94f5cffc5b7be93f6c3d3077ef24b9b83
Ahmad <crisitiano678@gmail.com>
2026-03-16 16:36:47 +0500

    main entry point updated for new claim

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI,HTTPException,Header,status
 from fastapi.middleware.cors import CORSMiddleware
 import json
-
+from domain.entities import ClaimEvent
 try:
-    from src.infrastructure.redis.redis_client import publish_event
     from src.infrastructure.supabase.supabase_client import get_user_from_token
 except ModuleNotFoundError:
-    from infrastructure.redis.redis_client import publish_event
+    from infrastructure.redis.redis_client import publish_to_stream
     from infrastructure.supabase.supabase_client import get_user_from_token
 
 app=FastAPI()
@@ -19,7 +18,7 @@ app.add_middleware(
 )
 
 @app.post("/publish_event",status_code=status.HTTP_202_ACCEPTED)
-async def publish_event_endpoint(event_channel:str,event_data:dict,authorization: str = Header(...)):
+async def publish_event_endpoint(event_data:dict,authorization: str = Header(...)):
     if not authorization:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Authorization header missing")
     
@@ -31,11 +30,13 @@ async def publish_event_endpoint(event_channel:str,event_data:dict,authorization
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Invalid token")
     
     #publish event to redis channel
-    result=publish_event(event_channel,event_data)
+    payload_data=ClaimEvent(claim_id=event_data.get("claim_id"),User_id=user.id).model_dump()
+    stream_name="stream:events:new_claims"
+    result=publish_to_stream(stream_name, payload_data)
     if not result:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail=result)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,detail="Failed to publish event")
 
     return{
         "status":"success",
-        "message":f"Event published to channel {event_channel}"
+        "message":f"Event published to stream {stream_name}"
     }
