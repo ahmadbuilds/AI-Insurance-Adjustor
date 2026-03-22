90e915a4d330eb6c2c326e3466038fd488ab8d4c
Ahmad <crisitiano678@gmail.com>
2026-03-22 06:23:01 +0500

    Entities updated

--- src/domain/entities.py
+++ src/domain/entities.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel,Field
 class ClaimEvent(BaseModel):
     claim_id:str=Field(description="Unique identifier for the claim")
     User_id:str=Field(description="Unique identifier for the user associated with the claim")
+
+
+#pydantic model for Safety guard of Vehicle Checking
+class isVehiclePresent(BaseModel):
+    present:bool=Field(description="Indicates whether the vehicle is present in the image or not",default=False)
