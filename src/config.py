e61fb599a567503d087e0ff7e49282cc5ccb850a
Ahmad <crisitiano678@gmail.com>
2026-04-04 13:52:24 +0500

    configuration file updated

--- src/config.py
+++ src/config.py
@@ -6,3 +6,4 @@ load_dotenv('.env.local')
 
 supabase_url = os.getenv('SUPABASE_URL')
 supabase_key = os.getenv('SUPABASE_ANON_KEY')
+supabase_service_role_key = os.getenv('SUPABASE_SERVICE_ROLE_KEY')
