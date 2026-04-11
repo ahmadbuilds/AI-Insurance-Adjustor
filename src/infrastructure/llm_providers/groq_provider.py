6de2a5f988fda324685994ea2266774af1a23a7e
Ahmad <crisitiano678@gmail.com>
2026-04-11 14:53:56 +0500

    groq provider fixed some bugs

--- src/infrastructure/llm_providers/groq_provider.py
+++ src/infrastructure/llm_providers/groq_provider.py
@@ -2,10 +2,11 @@ from langchain_groq import ChatGroq
 from langchain.chat_models import BaseChatModel
 
 #function to create a groq provider
-def create_model_instance(model_name:str='llama-3.3-70b-versatile', temperature:float=0, max_retries:int=3)->BaseChatModel:
+def create_model_instance(model_name:str='llama-3.3-70b-versatile', temperature:float=0, max_retries:int=3, **kwargs)->BaseChatModel:
     model=ChatGroq(
         model=model_name,
         temperature=temperature,
         max_retries=max_retries,
+        **kwargs,
     )
-    return model
+    return model
