260df1438db0968592a08c2ffc3e9388dbf5f76e
Ahmad <crisitiano678@gmail.com>
2026-04-12 18:17:39 +0500

    Chroma vector store instance creation function added

--- src/infrastructure/vector_store/chroma_client.py
+++ src/infrastructure/vector_store/chroma_client.py
@@ -2,7 +2,7 @@ from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 
 #function to create the instance of the vector store
-def create_chroma_instance(persist_dir:str='./data/policies')->Chroma:
+def create_chroma_instance(persist_dir:str='../data/policies')->Chroma:
     #creating Hugging Face Embedding model
     embedding_model=HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
 
